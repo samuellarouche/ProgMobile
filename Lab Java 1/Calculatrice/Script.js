a4d2a1a4e9bdd8ca0e050b7722345b3099cb85cb
samuellarouche <kildamar@hotmail.com>
2016-02-12 10:20:19 -0500

    Lab java 1

---
+++ Lab Java 1/Calculatrice/Script.js
@@ -0,0 +1,7 @@
+var Nb =( parseFloat(prompt("Entrez une expression arithmétique(+-*/) qui sera évaluer : ")));
+var Nb3 = (parseFloat(prompt("Entrez une expression arithmétique(+-*/) qui sera évaluer : ")));
+	console.log(eval("Nb + Nb3")); 
+	console.log(eval("Nb * Nb3")); 
+	console.log(eval("Nb - Nb3")); 
+	console.log(eval("Nb / Nb3")); 
+
