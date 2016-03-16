8993ac7f9cc79dd97d267c77062eb416fe885add
samuellarouche <kildamar@hotmail.com>
2016-03-16 09:47:16 -0400

    Fixing

--- Lab Java/Calculatrice/Script.js
+++
@@ -1,7 +0,0 @@
-var Nb =( parseFloat(prompt("Entrez une expression arithmétique(+-*/) qui sera évaluer : ")));
-var Nb3 = (parseFloat(prompt("Entrez une expression arithmétique(+-*/) qui sera évaluer : ")));
-	console.log(eval("Nb + Nb3")); 
-	console.log(eval("Nb * Nb3")); 
-	console.log(eval("Nb - Nb3")); 
-	console.log(eval("Nb / Nb3")); 
-
--- Lab Java/TableMultiple/Script.js
+++
@@ -1,6 +0,0 @@
-var Nb = parseFloat(prompt("Entrez un chiffre pour avoir la table de multiplication : "));
-
-for (var i = 0;i<=12;i++)
-{
-	console.log(Nb+"*"+i+"="+ (Nb*i));
-}
